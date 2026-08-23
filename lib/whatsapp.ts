e3fbaf7112681b6e0afe9da82b6ef982c88524ce
Mateusvanzuita <fotografo.vanzuita@gmail.com>
2026-08-23 13:41:06 -0300

    update

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMERO = "5547999999999" // TODO: substituir pelo número real
+export const WHATSAPP_NUMERO = "5511916731411" // TODO: substituir pelo número real
 
 export function linkWhatsapp(mensagem: string) {
   return `https://wa.me/${WHATSAPP_NUMERO}?text=${encodeURIComponent(mensagem)}`
